5a71469467619aa31dd2ba186b483fba69e8d2ea
Bashar Umar <basharu83@gmail.com>
2025-10-02 11:23:18 +0100

    added reausable infoBox

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 import Link from "next/link";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold mb-4"> Welcome to the Home Page </h1>
-      <Link href="/properties">Go to Properties</Link>
-    </div>
+    <>
+      <Hero />
+      <InfoBoxes />
+    </>
   );
 };
 
