25e7ddb43eb7a5acdf16fdd83d7af3e398a57812
Bashar Umar <basharu83@gmail.com>
2025-10-06 23:53:50 +0100

    added not found properties

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
 import Hero from "@/components/Hero";
+import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
-import Link from "next/link";
 
 const HomePage = () => {
+  console.log(process.env.MONGODB_URI);
+
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 };
