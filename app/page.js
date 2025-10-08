ba8c62a1867a2e2dc58da2e4714ae784d9e43d19
Bashar Umar <basharu83@gmail.com>
2025-10-08 02:46:18 +0100

    created property an user models

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import Hero from "@/components/Hero";
 import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
+import connectDB from "@/config/database";
 
+/**
+ * HomePage component that renders the hero, info boxes and home properties components.
+ */
 const HomePage = () => {
-  console.log(process.env.MONGODB_URI);
-
+  connectDB();
   return (
     <>
       <Hero />
