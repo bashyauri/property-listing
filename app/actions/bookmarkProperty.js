407aff6f99eec4b953c8278b8bd4b1c667fdfdda
Bashar Umar <basharu83@gmail.com>
2025-10-17 17:26:44 +0100

    added add and remove bookmark

---
+++ app/actions/bookmarkProperty.js
@@ -0,0 +1,33 @@
+"use server";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import User from "@/models/User";
+import getSessionUser from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+
+async function bookmarkProperty(propertyId) {
+  await connectDB();
+  const sessionUser = await getSessionUser();
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error("User ID is required");
+  }
+  const { userId } = sessionUser;
+  const user = await User.findById(userId);
+  let isBookmarked = user.bookmarks.includes(propertyId);
+  let message;
+  if (isBookmarked) {
+    // if already bookmarked remove it
+    user.bookmarks.pull(propertyId);
+    message = "Property removed from bookmarks";
+    isBookmarked = false;
+  } else {
+    // if not bookmarked add it
+    user.bookmarks.push(propertyId);
+    message = "Property added to bookmarks";
+    isBookmarked = true;
+  }
+  await user.save();
+  revalidatePath("/properties/saved", "page");
+  return { message, isBookmarked };
+}
+export default bookmarkProperty;
--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,7 +1,27 @@
+"use client";
 import { FaBookmark } from "react-icons/fa";
+import bookmarkProperty from "@/app/actions/bookmarkProperty";
+import { toast } from "react-toastify";
+import { useSession } from "next-auth/react";
 const BookmarkButton = ({ property }) => {
+  const { data: session } = useSession();
+  const userId = session?.user?.id;
+
+  const handleBookmark = async () => {
+    if (!userId)
+      return toast.error("You must be logged in to bookmark a property");
+    try {
+      const { message, isBookmarked } = await bookmarkProperty(property._id);
+      toast.success(message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
   return (
-    <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
+    <button
+      className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+      onClick={handleBookmark}
+    >
       <FaBookmark className="mr-1" /> Bookmark Property
     </button>
   );
