e5032a7b0ab85275cd02135dfad335f666a229b1
Bashar Umar <basharu83@gmail.com>
2025-10-24 11:39:08 +0100

    add pending message when submitting

--- components/PropertyContactForm.jsx
+++ components/PropertyContactForm.jsx
@@ -5,6 +5,7 @@ import { useActionState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { toast } from "react-toastify";
 import addMessage from "@/app/actions/addMessage";
+import SubmitMessageButton from "./SubmitMessageButton";
 const PropertyContactForm = ({ property }) => {
   const { data: session } = useSession();
   const [state, formAction] = useActionState(addMessage, {});
@@ -99,12 +100,7 @@ const PropertyContactForm = ({ property }) => {
             ></textarea>
           </div>
           <div>
-            <button
-              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline flex items-center justify-center"
-              type="submit"
-            >
-              <FaPaperPlane className="mr-2" /> Send Message
-            </button>
+            <SubmitMessageButton />
           </div>
         </form>
       </div>
