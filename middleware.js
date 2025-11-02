4b7e46a2c289600b4766aaac3394e17343cab488
Bashar Umar <basharu83@gmail.com>
2025-11-02 17:38:28 +0100

    update middleware

--- middleware.js
+++ middleware.js
@@ -1,23 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  // This is a NEGATIVE LOOKAHEAD regex pattern.
-  // It applies the middleware to ALL paths (*)
-  // EXCEPT for those inside the negative lookahead: (?!...)
-  matcher: [
-    "/((?!api/auth|_next/static|_next/image|favicon.ico|/|properties|about|register).*)",
-  ],
+  matcher: ["properties/add", "profile", "properties/saved", "messages"],
 };
-
-/*
-Explanation of Exclusions:
-1.  api/auth: NextAuth's own API routes (must be public)
-2.  _next/static: Static assets (JS, CSS, fonts)
-3.  _next/image: Image optimization files
-4.  favicon.ico: Browser icon
-5.  /: The homepage/root path
-6.  properties: An example of a public route you likely want excluded.
-7.  about / register: Any other public routes.
-
-*Make sure to include ALL your intended public paths in this exclusion list.*
-*/
